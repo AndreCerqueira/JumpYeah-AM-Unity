82d8c4605f5610557118af5d928c7bd7b5f72c30
André Cerqueira <78262628+AndreCerqueira@users.noreply.github.com>
2021-07-09 13:28:36 +0100

    platform generator

--- Assets/PlatformGenerator.cs
+++ Assets/PlatformGenerator.cs
@@ -4,33 +4,35 @@ using UnityEngine;
 
 public class PlatformGenerator : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject platform;
-
+    // Global Variables
+    [SerializeField] private GameObject platform;
+    private List<Vector2> platformPositionList = new List<Vector2>();
     public int plataformCount = 0;
     private int plataformCountMax = 30;
     private float minX, maxX;
     private float minY, maxY;
 
+
     // Start is called before the first frame update
     void Start() {
 
+        // Set data
         minX = GameObject.Find("minX").transform.position.x;
         maxX = GameObject.Find("maxX").transform.position.x;
         minY = GameObject.Find("minY").transform.position.y;
         maxY = GameObject.Find("maxY").transform.position.y;
 
         createInitialPlatforms();
-
-        StartCoroutine(autoCreatePlatforms());
     }
-
 
     // Update is called once per frame
-    void Update() {
- 
-    }
+    void Update()
+    {
 
+        if (plataformCount <= plataformCountMax) {
+            createPlatform();
+        }
+    }
 
     private void createInitialPlatforms() {
 
@@ -48,28 +50,30 @@ public class PlatformGenerator : MonoBehaviour
 
     private void createPlatform() 
     {
-        GameObject newPlatform = Instantiate(platform, new Vector2(
-            Random.Range(minX, maxX), 
-            Random.Range(minY, maxY)), 
-            Quaternion.identity
-        );
+        Vector2 posPlatform = Vector2.zero;
 
+        if (plataformCount > 0) {
+            
+            for (int i = 0; i < plataformCount; i++) {
+                
+                if (i == 0)
+                    posPlatform = new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxY));
+
+                while (Vector2.Distance(posPlatform, platformPositionList[i]) < 1) {
+                    posPlatform = new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxY));
+                } 
+
+            }
+        }
+        else {
+            posPlatform = new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxY));
+        }
+
+        GameObject newPlatform = Instantiate(platform, posPlatform, Quaternion.identity);
+        
+        platformPositionList.Add(newPlatform.transform.position);
         newPlatform.transform.parent = GameObject.Find("Platforms").transform;
         plataformCount++;
     } 
-
-
-    IEnumerator autoCreatePlatforms() {
-
-        while (true)
-        {
-            yield return new WaitForFixedUpdate();
-            
-            if (plataformCount <= plataformCountMax) {
-                createPlatform();
-            }
-        }
-    }
-
 
 }
